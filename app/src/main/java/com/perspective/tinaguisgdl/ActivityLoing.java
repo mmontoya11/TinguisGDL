2dc13ca7f8bb73ab3d77cf9edbe170366beb8bd5
mmontoya11 <PIMF123yo>
2019-10-29 11:43:56 -0600

    Configuracion login

--- app/src/main/java/com/perspective/tinaguisgdl/ActivityLoing.java
+++ app/src/main/java/com/perspective/tinaguisgdl/ActivityLoing.java
@@ -4,9 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.List;
 
 public class ActivityLoing extends AppCompatActivity {
+
+    private List<String> Inspectores = null;
+    private ArrayAdapter<String> adapter = null;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,16 @@ public class ActivityLoing extends AppCompatActivity {
         setContentView(R.layout.activity_loing);
 
         Button btn_Login = findViewById(R.id.button_login_ingresar);
+
+
+        Spinner spDireccion = findViewById(R.id.Spinner_Direccion);
+        spDireccion.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
 
         btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,4 +43,6 @@ public class ActivityLoing extends AppCompatActivity {
             }
         });
     }
+
+
 }
