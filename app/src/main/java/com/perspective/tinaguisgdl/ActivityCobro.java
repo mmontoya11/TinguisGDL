5908fd0b2c2543ad3c7a946e9d025ebf4cc024d8
Emmanuel <emmanuel.perezort@gmail.com>
2019-10-29 16:48:32 -0600

    Agregar asistencia calcular saldos

--- app/src/main/java/com/perspective/tinaguisgdl/ActivityCobro.java
+++ app/src/main/java/com/perspective/tinaguisgdl/ActivityCobro.java
@@ -1,6 +1,8 @@
 package com.perspective.tinaguisgdl;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,9 +11,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.perspective.tinaguisgdl.DB.GestionBD;
+
 public class ActivityCobro extends AppCompatActivity implements View.OnClickListener {
-     public TextView tvTianguis,tvNombre,tvCosto;
+     public TextView tvTianguis,tvNombre,tvCosto,tvFecha,tvMetro,tvCobro;
      public  Button btnQRscanner;
+     private String tianguis = "",nombre = "",fecha = "";
+     private GestionBD gestion = null;
+     private SQLiteDatabase db;
+     private int iTianguis = 0;
+     private double metros = 0d,total = 0d,costo = 0d;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,21 +33,39 @@ public class ActivityCobro extends AppCompatActivity implements View.OnClickList
 
         btnQRscanner.setOnClickListener(this);
 
+        gestion = new GestionBD(getApplicationContext(),"TianguisGDL",null,MainActivity.VERSION);
+
+        db = gestion.getReadableDatabase();
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        /*IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        if(result !=  null){
-            if(result.getContents() != null){
-                prueba.setText(result.getContents());
-                Log.e("Exito al escaner", "El escaner trajo algo");
-
-            }else{
-                Log.e("Fallo el escaner", "el escaner fallo y trajo "+ result);
+        if(requestCode == 0) {
+            if(resultCode == RESULT_OK) {
+                iTianguis = 0;
+                String contents = data.getStringExtra("SCAN_RESULT").trim();
+                System.err.println(contents);
+                String [] dato = contents.split("\\|");
+                System.err.println(contents.split("\\|")[3] + " da ");
+                if (dato.length > 0) {
+                    iTianguis = Integer.parseInt(dato[2]);
+                    tianguis = consultarTianguis(iTianguis);
+                    tvTianguis.setText(tianguis);
+                    nombre = dato[5].trim();
+                    tvNombre.setText(nombre);
+                    metros = Double.parseDouble(dato[0]);
+                    tvMetro.setText(dato[0]);
+                    fecha = dato[1];
+                    tvFecha.setText(fecha);
+                    costo = consultarCosto();
+                    tvCosto.setText(String.valueOf(costo));
+                    total = costo * Double.parseDouble(dato[0]);
+                    tvCobro.setText(String.valueOf(total));
+                }
             }
-        }*/
+        }
     }
 
     @Override
@@ -54,5 +81,29 @@ public class ActivityCobro extends AppCompatActivity implements View.OnClickList
                 break;
         }
 
+    }
+
+    public String consultarTianguis(int idTianguis) {
+        String tianguis = "";
+        String sql = "select nombre from " + GestionBD.TABLE_C_TIANGUIS + " where id = " + idTianguis;
+        Log.v("sql",sql);
+        Cursor cursor = null;
+        cursor =  db.rawQuery(sql,null);
+        if(cursor.moveToFirst()){
+            tianguis = cursor.getString(cursor.getColumnIndex("nombre"));
+        }
+        return tianguis;
+    }
+
+    public double consultarCosto() {
+        double costo = 0d;
+        String sql = "select costo_m from " + GestionBD.TABLE_CONFIGURACIONES;
+        Log.v("sql",sql);
+        Cursor cursor = null;
+        cursor =  db.rawQuery(sql,null);
+        if(cursor.moveToFirst()){
+            costo = cursor.getDouble(cursor.getColumnIndex("costo_m"));
+        }
+        return costo;
     }
 }
