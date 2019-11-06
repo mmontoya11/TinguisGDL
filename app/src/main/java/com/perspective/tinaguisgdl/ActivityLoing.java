fa0ddb8c489f4c7b5752cb35856cf75e1eb08073
Emmanuel <emmanuel.perezort@gmail.com>
2019-11-05 19:03:19 -0600

    login

--- app/src/main/java/com/perspective/tinaguisgdl/ActivityLoing.java
+++ app/src/main/java/com/perspective/tinaguisgdl/ActivityLoing.java
@@ -1,10 +1,8 @@
 package com.perspective.tinaguisgdl;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,19 +12,24 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.perspective.tinaguisgdl.DB.GestionBD;
+import com.perspective.tinaguisgdl.Model.Direccion;
+import com.perspective.tinaguisgdl.Model.Inspector;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ActivityLoing extends AppCompatActivity {
+public class ActivityLoing extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private ArrayList Inspectores = null;
-    private ArrayAdapter<String> adapterInspectores = null;
-
-
+    private Spinner spDireccion,spInspector;
+    private List<String> Inspectores = null;
+    private ArrayAdapter<String> adapterInspectores = null,adapter = null;
+    private List<Direccion> direcciones;
+    private List<String> direccion;
+    private List<Integer> idDireccion;
+    private GestionBD gestionBD = null;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +38,18 @@ public class ActivityLoing extends AppCompatActivity {
 
         final EditText etContraseña = findViewById(R.id.EditText_login_contraseña);
         Button btn_Login = findViewById(R.id.button_login_ingresar);
-        Spinner spDireccion = findViewById(R.id.Spinner_Direccion);
-        final Spinner spInspector = findViewById(R.id.Spinner_Inspector);
-        GestionBD gestionBD = new GestionBD(getApplicationContext(), "TianguisGDL", null, MainActivity.VERSION);
-        final SQLiteDatabase db = gestionBD.getReadableDatabase();
+        spDireccion = findViewById(R.id.Spinner_Direccion);
+        spInspector = findViewById(R.id.Spinner_Inspector);
+        gestionBD = new GestionBD(getApplicationContext(), "TianguisGDL", null, MainActivity.VERSION);
+        db = gestionBD.getReadableDatabase();
 
+        Inspectores = new ArrayList<>();
 
+        //Inspectores = NombresInspectores(db,1);
 
-        Inspectores = NombresInspectores(db);
-        adapterInspectores = new ArrayAdapter<>(this, R.layout.spinner_dropdown_layout,Inspectores);
+        adapterInspectores = new ArrayAdapter<>(getApplicationContext(), R.layout.spinner_dropdown_layout,Inspectores);
         spInspector.setAdapter(adapterInspectores);
+
 
         btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,29 +58,41 @@ public class ActivityLoing extends AppCompatActivity {
                 String Contraseña = etContraseña.getText().toString();
                 Log.e("Contraseña", Contraseña);
 
-                if(!Contraseña.isEmpty()){
+                if(gestionBD.ingresar(db,spInspector.getSelectedItem().toString(),etContraseña.getText().toString())) {
                     Intent intent = new Intent(ActivityLoing.this, MainActivity.class);
                     startActivity(intent);
-
                 }
 
             }
         });
+
+        direcciones = gestionBD.getAllDireccion("",db);
+        direccion = new ArrayList<>();
+        idDireccion = new ArrayList<>();
+
+        for (int i = 0;i < direcciones.size();i++) {
+            direccion.add(direcciones.get(i).getNombre());
+            idDireccion.add(direcciones.get(i).getId());
+        }
+
+        adapter = new ArrayAdapter<>(getApplicationContext(),R.layout.spinner_dropdown_layout,direccion);
+        spDireccion.setAdapter(adapter);
+
+        spDireccion.setOnItemSelectedListener(this);
     }
 
-    public ArrayList<String> NombresInspectores(SQLiteDatabase db){
-        Cursor cursor = db.rawQuery("select * from c_inspectores",null);
+    public void NombresInspectores(SQLiteDatabase db,int idDireccion){
+        Cursor cursor = db.rawQuery("select * from c_inspectores where c_dependencia_id = " + idDireccion,null);
         ArrayList<String> nombreInspectores = new ArrayList<>();
         String nombre;
-
+        Inspectores.clear();
         while (cursor.moveToNext()){
             nombre = cursor.getString(2) +" "+ cursor.getString(3) + " "+cursor.getString(4) ;
             nombreInspectores.add(nombre.toUpperCase());
+            Inspectores.add(nombre.toUpperCase());
             Log.e("Nombre inspectore.add", nombre);
-
         }
-
-        return nombreInspectores;
+        adapterInspectores.notifyDataSetChanged();
     }
     public boolean LoginValidation(SQLiteDatabase db, String username, String password){
         Log.e("LoginValidation User",username );
@@ -91,5 +108,28 @@ public class ActivityLoing extends AppCompatActivity {
             return true;
         }else
             return false;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.Spinner_Direccion:
+                Log.v("id d",idDireccion.get(position) + " ");
+                //Inspectores.clear();
+                /*Inspectores = gestionBD.getAllInspector("c_dependencia_id = " + idDireccion.get(position),db);
+                Log.v("inspectores", Inspectores.size() + " total");
+                for (int i = 0;i<Inspectores.size();i++) {
+                    Log.v("INSPECTOR", Inspectores.get(i));
+                }*/
+                NombresInspectores(db,idDireccion.get(position));
+                //spDireccion.setAdapter(adapter);
+                //adapterInspectores.notifyDataSetChanged();
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
