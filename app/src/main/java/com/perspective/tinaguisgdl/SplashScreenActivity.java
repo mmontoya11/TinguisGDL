d521b253d866b4119602a2d55a31cf0563cc71bd
mmontoya11 <PIMF123yo>
2019-11-12 11:10:55 -0600

    cambios en layout asistencia

---
+++ app/src/main/java/com/perspective/tinaguisgdl/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package com.perspective.tinaguisgdl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.perspective.tinaguisgdl.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
